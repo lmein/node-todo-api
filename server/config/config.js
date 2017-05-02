a314168f7efeee8629c3a0d838d113ee5a2234e1
lmein <lmeinecke@gmail.com>
2017-05-02 12:31:27 -0500

    Hide configuration information.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,26 @@
 //the following to differientiate between test(local), development(mocha), and production
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  //console.log(Object.keys(envConfig));
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+    //console.log(process.env[key]);
+  });
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
+
+//heroku config
+//heroku config:set JWT_SECRET=alfjajklsgfjdshgl - sets env variable JWT_SECRET to string
+//heroku get JWT_SECRET - gets the value of the JWT_SECRET env variable
+//heroku unset JWT_SECRET - removes the JWT_SECRET env variable
--- server/models/user.js
+++ server/models/user.js
@@ -47,7 +47,7 @@ UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
   //sign({object you want to sign}, 'secret value to salt hash')
-  var token = jwt.sign({_id: user._id.toHexString(), access}, '123abc').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({access, token});
 
@@ -72,7 +72,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, '123abc');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
